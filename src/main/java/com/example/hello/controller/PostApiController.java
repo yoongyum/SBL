631e95570f308be41db74ecb5ba389216b7f30ff
yoongyum <imshreg@naver.com>
2022-04-21 21:16:42 +0900

    feat: Learn POST MAPPING and create POST-DTO

---
+++ src/main/java/com/example/hello/controller/PostApiController.java
@@ -0,0 +1,19 @@
+package com.example.hello.controller;
+
+import com.example.hello.dto.PostRequestDto;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class PostApiController {
+
+    @PostMapping("/post")
+    public void post(@RequestBody PostRequestDto requsetData){
+        System.out.println(requsetData);
+    }
+}
