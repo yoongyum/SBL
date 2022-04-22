f3f509d421e33fa21a9f31a09512901e63ce9278
yoongyum <imshreg@naver.com>
2022-04-22 22:57:33 +0900

    feat: Learn PUT MAPPING and create PUT-DTO

---
+++ src/main/java/com/example/hello/dto/CarDto.java
@@ -0,0 +1,22 @@
+package com.example.hello.dto;
+
+public class CarDto {
+    private String name;
+    private String carNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "CarDto{" +
+                "name='" + name + '\'' +
+                ", carNumber='" + carNumber + '\'' +
+                '}';
+    }
+}
