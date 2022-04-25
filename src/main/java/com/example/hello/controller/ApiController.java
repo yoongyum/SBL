cdc1ba6e10bbd9faf1b8697d08819505c47c115f
yoongyum <imshreg@naver.com>
2022-04-25 15:57:36 +0900

    feat: Add req TEXT and Json format

--- src/main/java/com/example/hello/controller/ApiController.java
+++ src/main/java/com/example/hello/controller/ApiController.java
@@ -1,16 +1,26 @@
 package com.example.hello.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.hello.dto.User;
+import org.springframework.web.bind.annotation.*;
 
 @RestController //해당 class 는 REST API 처리하는 Controller
 @RequestMapping("/api")// RequestMapping URI를 지정해주는 Annotation
 public class ApiController {
 
-    @GetMapping("/hello")// http://localhost:9090/api/hello
-    public String hello(){
-        return "hello spring boot!";
+
+    //TEXT
+    @GetMapping("/text")
+    public String text(@RequestParam String account){
+        return account;
     }
+
+    //JSON
+    //req -> object mapper -> object -> method -> object -> object mapper -> json -> response
+    @PostMapping("/json")
+    public User json(@RequestBody User user){
+        System.out.println(user.toString());
+        return user;
+    }
+
 
 }
