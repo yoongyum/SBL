2f28a906f8f6a37e4132d15d76ab336f269b062b
yoongyum <imshreg@naver.com>
2022-04-27 21:24:20 +0900

    feat: Create Member, MemberRepository

--- src/main/java/com/example/hello/controller/PageController.java
+++ src/main/java/com/example/hello/controller/PageController.java
@@ -1,6 +1,6 @@
 package com.example.hello.controller;
 
-import com.example.hello.User;
+import com.example.hello.domain.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/com/example/hello/domain/User.java
+++ src/main/java/com/example/hello/domain/User.java
@@ -1,4 +1,4 @@
-package com.example.hello;
+package com.example.hello.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
