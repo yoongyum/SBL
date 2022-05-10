7fbe24d874b98687051ae45a0d0c4769d56c543d
yoongyum <imshreg@naver.com>
2022-05-10 10:37:08 +0900

    feat: Create HomeController
    컨트롤러가 정적 파일보다 우선순위가 높다.

--- src/main/java/com/example/hello/SpringConfig.java
+++ src/main/java/com/example/hello/SpringConfig.java
@@ -8,11 +8,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SpringConfig {
 
-    @Bean
-    public MemberService memberService(){
-        return new MemberService(memberRepository());
-    }
-
 
     @Bean
     public MemoryMemberRepository memberRepository(){
