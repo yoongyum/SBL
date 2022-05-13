133521b06220cd10521e66fd54c7c24d5db08d6b
yoongyum <imshreg@naver.com>
2022-05-13 23:20:13 +0900

    feat: Learn SpringData JPA

--- src/main/java/com/example/hello/SpringConfig.java
+++ src/main/java/com/example/hello/SpringConfig.java
@@ -1,33 +1,27 @@
 package com.example.hello;
 
-import com.example.hello.repository.JpaMemberRepository;
 import com.example.hello.repository.MemberRepository;
 import com.example.hello.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.persistence.EntityManager;
-import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
-    private EntityManager em;
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public SpringConfig(EntityManager em){
-        this.em = em;
+    public SpringConfig(MemberRepository memberRepository){
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService(){
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository(){
-        return new JpaMemberRepository(em);
-    }
 
 
 }
